332929ebf7fe5a78c32ba1694b7c4d5ad6fa95e5
nnapoleonn <nnapoleonn@mail.ru>
2018-03-11 20:36:50 +0300

    script finished

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,27 @@
+import re
+from string import punctuation
+from collections import Counter
+import sys
 
-
-def load_data(filepath):
-    pass
+def load_data(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file_handler:
+        return file_handler.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    the_processed_text = re.sub('[0-9{0}]'.format(punctuation), '', text.lower())
+    new_text = the_processed_text.split()
+    top_words = Counter(new_text)
+    top_words_limit = 10
+    return top_words.most_common(top_words_limit)
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 1:
+        sys.exit('Path to file is not found!')
+    file_path = sys.argv[1]
+    get_text = load_data(file_path)
+    most_frequent_words = get_most_frequent_words(get_text)
+    print('Top 10 words in text: ')
+    for word, amount in most_frequent_words:
+        print('{0}-{1}'.format(word, amount))
