3abe0654e8b4db8004d0c5a130ea0bf87f538fdb
nnapoleonn <35216297+nnapoleonn@users.noreply.github.com>
2018-03-12 19:37:54 +0200

    script finished

--- lang_frequency.py
+++ lang_frequency.py
@@ -3,6 +3,7 @@ from string import punctuation
 from collections import Counter
 import sys
 
+
 def load_data(file_path):
     with open(file_path, 'r', encoding='utf-8') as file_handler:
         return file_handler.read()
@@ -10,18 +11,21 @@ def load_data(file_path):
 
 def get_most_frequent_words(text):
     the_processed_text = re.sub('[0-9{0}]'.format(punctuation), '', text.lower())
-    new_text = the_processed_text.split()
-    top_words = Counter(new_text)
+    new_list = the_processed_text.split()
+    words = Counter(new_list)
     top_words_limit = 10
-    return top_words.most_common(top_words_limit)
+    return words.most_common(top_words_limit)
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         sys.exit('Path to file is not found!')
     file_path = sys.argv[1]
-    get_text = load_data(file_path)
-    most_frequent_words = get_most_frequent_words(get_text)
-    print('Top 10 words in text: ')
-    for word, amount in most_frequent_words:
-        print('{0}-{1}'.format(word, amount))
+    try:
+        get_text = load_data(file_path)
+        most_frequent_words = get_most_frequent_words(get_text)
+        print('Top 10 words in text: ')
+        for word, amount in most_frequent_words:
+            print('{0}-{1}'.format(word, amount))
+    except FileNotFoundError:
+        sys.exit('File not found')
